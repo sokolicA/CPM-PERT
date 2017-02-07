1b0636bc9f542debe12ebf30c9328427d6ea693d
Manca Cör <manca.cor@student.fmf.uni-lj.si>
2017-02-07 14:15:48 +0100

    Topolosko urejanje

--- CPM-PERT model/Program.R
+++ CPM-PERT model/Program.R
@@ -1,7 +1,8 @@
 Opr<-c(1,2,3,4)
 Pred<-list(c(0),c(1),c(0),c(2,3))
-
-matrika.graf<-function(Opr, Pred, Cas){
+Opr1<-(1:12)
+Pred1<-list(c(0),c(0),c(0),c(1,2),c(4),c(3),c(5,6),c(7),c(8),c(7),c(10),c(10))
+Adj<-function(Opr, Pred){
   z=length(Opr)+1
   NO<-c(0, Opr,z)
   A<-matrix(nrow=z+1,ncol=z+1)
@@ -28,3 +29,25 @@ matrika.graf<-function(Opr, Pred, Cas){
     
   return(A)
 }
+top.sort<-function(Opr,adj){
+  Q<-list()
+  i<-1
+  st<-c()
+  for(v in Opr){
+    st[v]<-sum(adj[,v])
+  }
+  for(v in Opr){
+    if (st[v]==0){
+      Q[[i]]<-v
+      i<-i+1
+    }
+  }
+}
+
+
+
+
+
+
+
+
